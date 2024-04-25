0d51311fc424f2384fbd445638ad73e38d50ccc2
Rodolfo Solorzano <xofo.so@gmail.com>
2024-04-26 00:17:11 +0200

    mapeando errores en rutas

--- Library/router.php
+++ Library/router.php
@@ -2,6 +2,7 @@
 
 namespace Library;
 
+use Error;
 use Exception;
 use Library\ApiController;
 
@@ -10,18 +11,32 @@ class Router
     public static function manage($url)
     {
         $url = ltrim($url, '/');
+        $url = rtrim($url, '/');
         $segments = explode('/', $url);
 
         // Construcción de la ruta:  Primer segmento: api.  Segundo segmento: método.  Tercer segmento: parámetro.
         // Ejemplo: api/getTask/1
         try{
-            if(!isset($segments[2]))
+            if(empty($segments[0])){
+                echo "Index";
+                return;
+            }
+            if(!isset($segments[1])){
+                throw new Error();
+            }
+            if(!isset($segments[2])){
                 echo call_user_func([ApiController::class, $segments[1]]);
-            else
-                echo call_user_func([ApiController::class, $segments[1]], $segments[2]);
+                return;
+            }
+            
+            echo call_user_func([ApiController::class, $segments[1]], $segments[2]);
+            return;
         }catch(Exception $e){
             http_response_code(400);
             echo "Bad Request: ".$e->getMessage();
+        }catch(Error $e){
+            http_response_code(404);
+            echo "Page Not Found";
         }
         
 
